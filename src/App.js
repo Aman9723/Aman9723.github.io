8483b7101b1025c747b655155c976d0e7a92a534
Aman Singh Chauhan <amansingh9723chauhan@gmail.com>
2022-10-13 13:32:51 +0530

    added boilerplate code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hi, I am Aman. Yet to build this site.
+    </div>
+  );
+}
+
+export default App;
